1c7141b5b6459961812dc4c74758d2680405fdc9
RishiTiwari18 <rishiitiwarii18@gmail.com>
2026-08-23 11:52:17 +0530

    Time: 36 ms (38.84%), Space: 36.5 MB (57.33%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -12,8 +12,31 @@ int find(int index,int amount,vector<int>&coins,int n ,vector<vector<int>>&dp)
     return dp[index][amount]=find(index,amount-coins[index],coins,n,dp)+find(index+1,amount,coins,n,dp);
 }
     int change(int amount, vector<int>& coins) {
-        int n=coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        return find(0,amount,coins,n,dp);
+     int n = coins.size();
+
+    vector<vector<int>> dp(
+        n + 1,
+        vector<int>(amount + 1, 0)
+    );
+
+    for (int i = 0; i <= n; i++)
+        dp[i][0] = 1;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= amount; j++) {
+
+            dp[i][j] = dp[i - 1][j];
+
+            if (coins[i - 1] <= j) {
+                if (dp[i][j - coins[i - 1]] > INT_MAX - dp[i - 1][j])
+                    dp[i][j] = INT_MAX;
+                else
+                    dp[i][j] += dp[i][j - coins[i - 1]];
+            }
+        }
+    }
+
+    return dp[n][amount];
+
     }
 };
