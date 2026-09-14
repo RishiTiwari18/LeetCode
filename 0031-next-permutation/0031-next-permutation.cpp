2f09470af5a6fc99d94850cc426bdfdd397e9948
RishiTiwari18 <rishiitiwarii18@gmail.com>
2026-09-14 07:04:09 +0530

    Time: 0 ms (100%), Space: 15.6 MB (82.74%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        // 1. Find pivot
+        int i = n - 2;
+
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        // 2. Find next greater element and swap
+        if (i >= 0) {
+            int j = n - 1;
+
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+
+            swap(nums[i], nums[j]);
+        }
+
+        // 3. Reverse the right part
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
